770ad7346449268a246835f1e0c1eaf4cc370d39
OakBricks <oakbricks@gmail.com>
2021-08-03 15:31:18 -0500

    more shit

--- src/main/java/org/oakbricks/oakores/ExampleMod.java
+++ src/main/java/org/oakbricks/oakores/ExampleMod.java
@@ -1,6 +1,5 @@
-package com.example.examplemod;
+package org.oakbricks.oakores;
 
-import net.minecraft.world.item.Items;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -39,16 +38,15 @@ public class ExampleMod {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        LOGGER.info("Hello from common setup! This is *after* registries are done, so we can do this:");
-        LOGGER.info("Look, I found a {}!", Items.DIAMOND.getRegistryName());
+
     }
 
     private void clientSetup(final FMLClientSetupEvent event) {
-        LOGGER.info("Hey, we're on Minecraft version {}!", event.getMinecraftSupplier().get().getLaunchedVersion());
+        // LOGGER.info("Hey, we're on Minecraft version {}!", event.getMinecraftSupplier().get().getLaunchedVersion());
     }
 
     @SubscribeEvent
     public void kaboom(ExplosionEvent.Detonate event) {
-        LOGGER.info("Kaboom! Something just blew up in {}!", event.getWorld());
+        // LOGGER.info("Kaboom! Something just blew up in {}!", event.getWorld());
     }
 }
